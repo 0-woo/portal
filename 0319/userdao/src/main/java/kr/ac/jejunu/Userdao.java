7dfd755eb3dae5f59778be7bb8ae5a7a86f4d75d
0-woo <dudndp678@naver.com>
2022-03-20 01:44:58 +0900

    0319 userdao review

---
+++ 0319/userdao/src/main/java/kr/ac/jejunu/Userdao.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+public class Userdao {
+    public User findById(Integer id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/portal",
+                "root",
+                "Woo971015!"
+        );
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "select*from userinfo where id = ?"
+        );
+        preparedStatement.setInt(1,id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return user;
+    }
+}
