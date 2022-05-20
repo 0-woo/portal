af56f8cb0eeb4b7ff132f22fd5ad9322a337c6db
0-woo <dudndp678@naver.com>
2022-05-20 16:26:00 +0900

    0520-1

--- userdao/src/main/java/kr/ac/jejunu/UserFilter.java
+++ userdao/src/main/java/kr/ac/jejunu/UserFilter.java
@@ -6,16 +6,20 @@ import java.io.IOException;
 public class UserFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("******* Filter init ***********");
         Filter.super.init(filterConfig);
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
+        System.out.println("******* Filter before ***********");
+        chain.doFilter(request, response);
+        System.out.println("******* Filter after ***********");
     }
 
     @Override
     public void destroy() {
+        System.out.println("******* Filter destroy ***********");
         Filter.super.destroy();
     }
 }
